1b74dbe48bab57873fd88db2605a2557dbf6e9c1
Thomas Manning <thomas@webprogrammers.com>
2014-09-27 03:40:15 -0400

    Allowed fs to parse mount option
    Added code to recognize mount options and put them into a format that
    fusepy could understand and pass along to fuse.

--- __main__.py
+++ __main__.py
@@ -3,12 +3,27 @@ import fuse
 from hide_symlinks import HideSymlinks
 
 def main():
-	if len(sys.argv) != 3:
+	if len(sys.argv) < 3:
 		print 'usage: {} <root> <mountpoint>'.format(sys.argv[0])
 		exit(1)
 
+        # Find the filesystem options (the things following "-o")
+        options = ""
+        for i in range(len(sys.argv)):
+            if sys.argv[i] == "-o" and len(sys.argv) > i+1:
+                options = sys.argv[i+1]
+
+        # Turn them into a format fuse.FUSE can handle (kwargs)
+        fuse_args = {}
+        for option in options.split(","):
+            option_parts = option.split("=")
+            if len(option_parts) == 1:
+                fuse_args[option_parts[0]] = True
+            else:
+                fuse_args[option_parts[0]] = option_parts[1]
+
 	# Start the FUSE FS
-	fuse.FUSE(HideSymlinks(sys.argv[1]), sys.argv[2], foreground=True)
+        fuse.FUSE(HideSymlinks(sys.argv[1]), sys.argv[2], foreground=False, **fuse_args)
 
 if __name__ == '__main__':
 	main()
