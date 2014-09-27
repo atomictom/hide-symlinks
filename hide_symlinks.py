522425764ccb4fba6cd8accb3c5e98e1bd2471b6
Thomas Manning <thomas@webprogrammers.com>
2014-09-27 03:40:09 -0400

    minor formatting change

--- hide_symlinks.py
+++ hide_symlinks.py
@@ -11,5 +11,5 @@ class HideSymlinks(Loopback):
 		# This is the key line
 		st = os.stat(path)
 		keys = ('st_atime', 'st_ctime','st_gid', 'st_mode',
-			   'st_mtime', 'st_nlink', 'st_size', 'st_uid')
+			'st_mtime', 'st_nlink', 'st_size', 'st_uid')
 		return dict((key, getattr(st, key)) for key in keys)
