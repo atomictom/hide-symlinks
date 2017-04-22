ae21c4fb58b306326b8a57b97b6dbbe3d58041f2
Thomas Manning <thomas@webprogrammers.com>
2017-04-22 17:04:18 +0100

    Fix formatting

--- hide_symlinks.py
+++ hide_symlinks.py
@@ -5,11 +5,11 @@ from loopback import Loopback
 
 class HideSymlinks(Loopback):
 
-	symlink = None
+    symlink = None
 
-	def getattr(self, path, fh=None):
-		# This is the key line
-		st = os.stat(path)
-		keys = ('st_atime', 'st_ctime','st_gid', 'st_mode',
-			'st_mtime', 'st_nlink', 'st_size', 'st_uid')
-		return dict((key, getattr(st, key)) for key in keys)
+    def getattr(self, path, fh=None):
+        # This is the key line
+        st = os.stat(path)
+        keys = ('st_atime', 'st_ctime','st_gid', 'st_mode',
+                'st_mtime', 'st_nlink', 'st_size', 'st_uid')
+        return dict((key, getattr(st, key)) for key in keys)
